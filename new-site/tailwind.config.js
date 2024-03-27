056381666651db6a83abd3d40349a1e69fefc6c4
am-mokhtari <you@example.com>
2024-03-27 21:03:57 +0330

    added new screen sizes to config

--- new-site/tailwind.config.js
+++ new-site/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./**/*.{html,js}"],
+    theme: {
+        screens: {
+            's': '340px',
+            'mid': '460px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
+        extend: {},
+    },
+    plugins: [],
 }
-
